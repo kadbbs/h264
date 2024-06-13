db4203b8b5a3ee9f16259a0668614aff9022bacc
kadbbs <2924428540@qq.com>
2024-06-13 23:29:47 +0800

    first commit add README.md

---
+++ cmake-build-debug/CMakeFiles/h264.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/h264.dir/main.cpp.o"
+  "h264"
+  "h264.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/h264.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ md/H.264_AVC.md
@@ -0,0 +1,3 @@
+![img.png](img.png)
+
+H264/AVC发展历史
